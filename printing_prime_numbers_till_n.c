710ef6cad3066778d6697e0fbbdb7ce832feaaf7
ee20btech11050 <108258359+ee20btech11050@users.noreply.github.com>
2022-06-27 13:08:30 +0530

    Create printing_prime_numbers_till_n.c

---
+++ printing_prime_numbers_till_n.c
@@ -0,0 +1,30 @@
+// Prime Number Check
+
+#include <stdio.h>
+
+int main(void){
+  int num;
+  printf("Enter a positive number to print all the prime numbers until that number is encountered: ");
+  scanf("%d",&num);
+  
+
+  for(int j=2;j<=num;j++)
+  {
+    int count = 0;
+    for(int i=2;i<j;i++)
+    {
+      if((j%i) == 0)
+      {
+        count = 1;
+        break;
+      }
+    }
+
+    if(!count)
+    {
+      printf("%d\n",j);
+    }  
+  }
+
+  return 0;
+}
